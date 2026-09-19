caa9c05fe1609c846539e8b35bed9b7f5a89dfa7
ckor0812 <christopherkoril@Mac.lan>
2026-09-20 04:05:29 +1000

    Reduce piece byte size to 2 and assert, rename None to Empty

--- src/types.h
+++ src/types.h
@@ -1,20 +1,23 @@
 #pragma once
 #include <cassert>
+#include <cstdint>
 
 inline constexpr int RANKS = 8;
 inline constexpr int FILES = 8;
 
-enum class PieceType {Pawn, Bishop, Knight, Rook, Queen, King, None};
-enum class PieceColour {White, Black, None};
+enum class PieceType : std::uint8_t {Pawn, Bishop, Knight, Rook, Queen, King, Empty};
+enum class PieceColour : std::uint8_t {White, Black, Empty};
 
 struct Piece {
-    PieceType pieceType;
-    PieceColour pieceColour;
+    PieceType pieceType = PieceType::Empty;
+    PieceColour pieceColour = PieceColour::Empty;
 
     bool isEmpty() const{
-        bool isEmptyPiece = (pieceType == PieceType::None);
-        bool isEmptyColour = (pieceColour == PieceColour::None);
+        bool isEmptyPiece = (pieceType == PieceType::Empty);
+        bool isEmptyColour = (pieceColour == PieceColour::Empty);
         assert(isEmptyPiece == isEmptyColour);
         return isEmptyPiece && isEmptyColour;
     }
 };
+
+static_assert(sizeof(Piece) == 2);
