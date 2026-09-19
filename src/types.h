d79835ab313e8701c0c56f898fba82a6826cc606
ckor0812 <christopherkoril@Mac.lan>
2026-09-20 01:47:18 +1000

    Implement constant FILES and RANKS variable in type.h, shrink piece byte size to 2 and assert

--- src/types.h
+++ src/types.h
@@ -1,16 +1,19 @@
 #pragma once
 #include <cassert>
 
-enum class PieceType {Pawn, Bishop, Knight, Rook, Queen, King, EmptyPiece};
-enum class PieceColour {White, Black, EmptyColour};
+inline constexpr int RANKS = 8;
+inline constexpr int FILES = 8;
+
+enum class PieceType {Pawn, Bishop, Knight, Rook, Queen, King, None};
+enum class PieceColour {White, Black, None};
 
 struct Piece {
     PieceType pieceType;
     PieceColour pieceColour;
 
     bool isEmpty() const{
-        bool isEmptyPiece = (pieceType == PieceType::EmptyPiece);
-        bool isEmptyColour = (pieceColour == PieceColour::EmptyColour);
+        bool isEmptyPiece = (pieceType == PieceType::None);
+        bool isEmptyColour = (pieceColour == PieceColour::None);
         assert(isEmptyPiece == isEmptyColour);
         return isEmptyPiece && isEmptyColour;
     }
