63af0a4c0131f1c469dd5ac0efa508cb95a1d518
ralyraly1 <kirstenkoril@gmail.com>
2026-09-21 01:41:45 +1000

    Implement bit flags for castling rights

--- src/types.h
+++ src/types.h
@@ -5,6 +5,11 @@
 inline constexpr int RANKS = 8;
 inline constexpr int FILES = 8;
 
+inline constexpr std::uint8_t BLACK_QUEENSIDE_CASTLE = 0b0001;
+inline constexpr std::uint8_t BLACK_KINGSIDE_CASTLE = 0b0010;
+inline constexpr std::uint8_t WHITE_QUEENSIDE_CASTLE = 0b0100;
+inline constexpr std::uint8_t WHITE_KINGSIDE_CASTLE = 0b1000;
+
 enum class PieceType : std::uint8_t {Pawn, Bishop, Knight, Rook, Queen, King, Empty};
 enum class PieceColour : std::uint8_t {White, Black, Empty};
 enum class Side : std::uint8_t {White, Black};
